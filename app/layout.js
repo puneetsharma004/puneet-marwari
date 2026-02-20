4f1e654ce5faa9a806d072d8c9eb2c9c4253abc7
satindersingh021 <satinder.singh021@gmail.com>
2026-02-20 18:43:42 +0530

    Deployment Fix

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,6 @@
 import ScrollToTop from "@/components/common/ScrollToTop";
 import "../public/css/style.css";
 import { Josefin_Sans, Inter } from "next/font/google";
-import ScrollTopBehaviour from "@/components/common/ScrollTopBehavier";
-import Wrapper from "@/components/layout/Wrapper";
-import BootstrapClient from "@/components/common/BootstrapClient";
 
 const josefin = Josefin_Sans({
   weight: ["400", "500", "700"],
@@ -19,14 +16,18 @@ const inter = Inter({
   display: "swap",
 });
 
+import ScrollTopBehaviour from "@/components/common/ScrollTopBehavier";
+import Wrapper from "@/components/layout/Wrapper";
+
+if (typeof window !== "undefined") {
+  import("bootstrap");
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {/* Avoid empty head tags if not needed, or add metadata here */}
-      </head>
-      <body className={`${josefin.variable} ${inter.variable}`}>
-        <BootstrapClient />
+      <head></head>
+      <body className={`${josefin.className} ${inter.className}`}>
         <Wrapper>{children}</Wrapper>
         <ScrollToTop />
         <ScrollTopBehaviour />
