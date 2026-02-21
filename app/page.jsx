9a8a334e5d4c163023e0f92bf3dc576ff1c5451a
Puneet <checkout206@gmail.com>
2026-02-21 18:27:26 +0530

    removed (home)

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import Home from "./(homes)/page";
-import Header from "@/components/dasboard/Header";
-import Hero8 from "@/components/homes/heros/Hero8";
-import PopularTourSlider from "@/components/homes/tours/PopulerTourSlider";
-import Features6 from "@/components/homes/features/Features6";
-import DestinationsTwo from "@/components/homes/destinations/DestinationsTwo";
 import BannerTwo from "@/components/homes/banners/BannerTwo";
-import TestimonialOne from "@/components/homes/testimonials/TestimonialOne";
 import BrandsOne from "@/components/homes/brands/BrandsOne";
-import Footer from "react-multi-date-picker/plugins/range_picker_footer";
+import DestinationsTwo from "@/components/homes/destinations/DestinationsTwo";
+import Features6 from "@/components/homes/features/Features6";
+import Hero8 from "@/components/homes/heros/Hero8";
+import TestimonialOne from "@/components/homes/testimonials/TestimonialOne";
+import PopularTourSlider from "@/components/homes/tours/PopulerTourSlider";
+import Footer from "@/components/layout/footers/Footer";
+import Header from "@/components/layout/header/Header";
+import React from "react";
+
+
 
 export const metadata = {
   title: "Home-1 || ViaTour - Travel & Tour React NextJS Template",
@@ -19,16 +20,16 @@ export default function page() {
   return (
     <>
       <Header />
-              <Hero8 />
-              <PopularTourSlider />
-              <Features6 />
-              <DestinationsTwo />
-              <BannerTwo />
-              <div className="bg-accent-1-05">
-                <TestimonialOne />
-              </div>
-              <BrandsOne />
-              <Footer />
+      <Hero8 />
+      <PopularTourSlider />
+      <Features6 />
+      <DestinationsTwo />
+      <BannerTwo />
+      <div className="bg-accent-1-05">
+        <TestimonialOne />
+      </div>
+      <BrandsOne />
+      <Footer />
     </>
   );
 }
