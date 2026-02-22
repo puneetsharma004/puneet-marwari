9f35f8cdc30741e0837c7fe3570c4bb149f7d137
Puneet <checkout206@gmail.com>
2026-02-22 22:41:16 +0530

    meta data changed

--- app/page.jsx
+++ app/page.jsx
@@ -12,8 +12,8 @@ import React from "react";
 
 
 export const metadata = {
-  title: "Home-1 || ViaTour - Travel & Tour React NextJS Template",
-  description: "ViaTour - Travel & Tour React NextJS Template",
+  title: "Mawari - Travel & Tour",
+  description: "Mawari - Travel & Tour",
 };
 
 export default function page() {
