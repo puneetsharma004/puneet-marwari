3fa57f70749026279cdd43827ae650d70ce46e9f
Puneet <checkout206@gmail.com>
2026-03-21 21:24:24 +0530

    square mawari 2

--- components/common/ModalVideo.jsx
+++ components/common/ModalVideo.jsx
@@ -23,7 +23,7 @@ const ModalVideo = ({ videoId = "L61p2uyiMSo", isOpen, setIsOpen, src }) => {
                     ? src
                     : `https://www.youtube.com/embed/${videoId}?autoplay=1`
                 }
-                title="YouTube video player"
+                title="YouTube video player "
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
                 style={iframeStyle}
